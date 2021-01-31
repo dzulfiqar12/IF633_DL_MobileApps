92c1bd200cc13365d92357dc3d71fdfc7fb32cd1
dzulfiqar12 <78007781+dzulfiqar12@users.noreply.github.com>
2021-01-31 22:45:27 +0700

    Week01

---
+++ Week01_32892/app/src/main/java/com/umn/MainActivity.java
@@ -0,0 +1,13 @@
+package com.umn;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
